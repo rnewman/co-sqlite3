872c98f02be508917e1d9fb627af7e0e7cf70214
Richard Newman <rnewman@twinql.com>
2016-04-15 18:37:22 -0700

    Fork, reformat, rename.

--- index.js
+++ index.js
@@ -1,271 +1,284 @@
 /**
- * promise based node-sqlite3 for co or koa
+ * ES6 promise-based node-sqlite3.
  */
 
-var sqlite3 = require('sqlite3').verbose();
-var Promise = require('bluebird');
+const sqlite3 = require('sqlite3').verbose();
 
-module.exports = function(filename, mode) {
+module.exports = function (filename, mode) {
 
-	/*
-	 * in node-sqlite3  include  database and statement 
-	 */
-	function CoSqlite3(db) {
-		this.db = db;
-	}
+    /*
+     * in node-sqlite3  include  database and statement
+     */
+    function PSQLite3(db) {
+        this.db = db;
+    }
 
-	function Statement(statement) {
-		this.statement = statement;
-	}
+    function Statement(statement) {
+        this.statement = statement;
+    }
 
-	CoSqlite3.prototype = {
-		close: function() {
-			var that = this;
-			return new Promise(function(resolve, reject) {
-				that.db.close(function(err) {
-					if (err) {
-						reject(err);
-					}
-				});
+    PSQLite3.prototype = {
+        close: function () {
+            const that = this;
+            return new Promise(function (resolve, reject) {
+                that.db.close(function (err) {
+                    if (err) {
+                        reject(err);
+                    }
+                });
 
-				that.db.on('close', function() {
-					resolve();
-				});
-			});
-		},
-		run: function() {
-			var that = this;
-			var args = Array.prototype.slice.call(arguments, 0);
+                that.db.on('close', function () {
+                    resolve();
+                });
+            });
+        },
 
-			return new Promise(function(resolve, reject) {
-				args.push(function(err) {
-					if (err) {
-						reject(err);
-					}
-					resolve(this);
-				});
+        run: function () {
+            var that = this;
+            var args = Array.prototype.slice.call(arguments, 0);
 
-				that.db.run.apply(that.db, args);
-			});
-		},
-		get: function() {
-			var that = this;
-			var args = Array.prototype.slice.call(arguments, 0);
+            return new Promise(function (resolve, reject) {
+                args.push(function (err) {
+                    if (err) {
+                        reject(err);
+                    }
+                    resolve(this);
+                });
 
-			return new Promise(function(resolve, reject) {
-				args.push(function(err, row) {
-					if (err) {
-						reject(err);
-					}
-					resolve(row);
-				});
+                that.db.run.apply(that.db, args);
+            });
+        },
 
-				that.db.get.apply(that.db, args);
-			});
-		},
-		all: function() {
-			var that = this;
-			var args = Array.prototype.slice.call(arguments, 0);
+        get: function () {
+            var that = this;
+            var args = Array.prototype.slice.call(arguments, 0);
 
-			return new Promise(function(resolve, reject) {
-				args.push(function(err, rows) {
-					if (err) {
-						reject(err);
-					}
-					resolve(rows);
-				});
+            return new Promise(function (resolve, reject) {
+                args.push(function (err, row) {
+                    if (err) {
+                        reject(err);
+                    }
+                    resolve(row);
+                });
 
-				that.db.all.apply(that.db, args);
-			});
-		},
-		each: function() {
-			var that = this;
-			var args = Array.prototype.slice.call(arguments, 0);
-			return new Promise(function(resolve, reject) {
-				var lastArgs = args[args.length - 1];
-				if (typeof lastArgs !== 'function') {
-					args.push(function(err, row) {
-						if (err) {
-							reject(err);
-						}
+                that.db.get.apply(that.db, args);
+            });
+        },
 
-						// TODO: how to reslove each row
-					});
-				}
-				// complete callback
-				args.push(function(err, num) {
-					if (err) {
-						reject(err);
-					}
-					resolve(num);
-				});
-				that.db.each.apply(that.db, args);
+        all: function () {
+            var that = this;
+            var args = Array.prototype.slice.call(arguments, 0);
 
-			});
+            return new Promise(function (resolve, reject) {
+                args.push(function (err, rows) {
+                    if (err) {
+                        reject(err);
+                    }
+                    resolve(rows);
+                });
 
-		},
-		exec: function() {
-			var that = this;
-			var args = Array.prototype.slice.call(arguments, 0);
+                that.db.all.apply(that.db, args);
+            });
+        },
 
-			return new Promise(function(resolve, reject) {
-				args.push(function(err) {
-					if (err) {
-						reject(err);
-					}
-					resolve();
-				});
+        each: function () {
+            var that = this;
+            var args = Array.prototype.slice.call(arguments, 0);
+            return new Promise(function (resolve, reject) {
+                var lastArgs = args[args.length - 1];
+                if (typeof lastArgs !== 'function') {
+                    args.push(function (err, row) {
+                        if (err) {
+                            reject(err);
+                        }
 
-				that.db.exec.apply(that.db, args);
-			});
-		},
-		prepare: function() {
-			var that = this;
-			var args = Array.prototype.slice.call(arguments, 0);
-			var statement;
+                        // TODO: how to resolve each row?
+                    });
+                }
 
-			return new Promise(function(resolve, reject) {
-				args.push(function(err) {
-					if (err) {
-						reject(err);
-					}
-					resolve(new Statement(statement));
-				});
+                // complete callback
+                args.push(function (err, num) {
+                    if (err) {
+                        reject(err);
+                    }
+                    resolve(num);
+                });
 
-				statement = that.db.prepare.apply(that.db, args);
-			});
-		}
-	};
-	Statement.prototype = {
-		bind: function() {
-			var that = this;
-			var args = Array.prototype.slice.call(arguments, 0);
+                that.db.each.apply(that.db, args);
+            });
+        },
 
-			return new Promise(function(resolve, reject) {
-				args.push(function(err) {
-					if (err) {
-						reject(err);
-					}
-					resolve();
-				});
+        exec: function () {
+            var that = this;
+            var args = Array.prototype.slice.call(arguments, 0);
 
-				that.statement.bind.apply(that.statement, args);
-			});
-		},
-		reset: function() {
-			var that = this;
-			var args = Array.prototype.slice.call(arguments, 0);
+            return new Promise(function (resolve, reject) {
+                args.push(function (err) {
+                    if (err) {
+                        reject(err);
+                    }
+                    resolve();
+                });
 
-			return new Promise(function(resolve, reject) {
-				args.push(function(err) {
-					if (err) {
-						reject(err);
-					}
-					resolve();
-				});
+                that.db.exec.apply(that.db, args);
+            });
+        },
 
-				that.statement.reset.apply(that.statement, args);
-			});
-		},
-		finalize: function() {
-			var that = this;
-			var args = Array.prototype.slice.call(arguments, 0);
+        prepare: function () {
+            var that = this;
+            var args = Array.prototype.slice.call(arguments, 0);
+            var statement;
 
-			return new Promise(function(resolve, reject) {
-				args.push(function() {
-					resolve();
-				});
+            return new Promise(function (resolve, reject) {
+                args.push(function (err) {
+                    if (err) {
+                        reject(err);
+                    }
+                    resolve(new Statement(statement));
+                });
 
-				that.statement.finalize.apply(that.statement, args);
-			});
-		},
-		run: function() {
-			var that = this;
-			var args = Array.prototype.slice.call(arguments, 0);
+                statement = that.db.prepare.apply(that.db, args);
+            });
+        }
+    };
 
-			return new Promise(function(resolve, reject) {
-				args.push(function(err) {
-					if (err) {
-						reject(err);
-					}
-					resolve(this);
-				});
+    Statement.prototype = {
+        bind: function () {
+            var that = this;
+            var args = Array.prototype.slice.call(arguments, 0);
 
-				that.statement.run.apply(that.statement, args);
-			});
-		},
-		get: function() {
-			var that = this;
-			var args = Array.prototype.slice.call(arguments, 0);
+            return new Promise(function (resolve, reject) {
+                args.push(function (err) {
+                    if (err) {
+                        reject(err);
+                    }
+                    resolve();
+                });
 
-			return new Promise(function(resolve, reject) {
-				args.push(function(err, row) {
-					if (err) {
-						reject(err);
-					}
-					resolve(row);
-				});
+                that.statement.bind.apply(that.statement, args);
+            });
+        },
 
-				that.statement.get.apply(that.statement, args);
-			});
-		},
-		all: function() {
-			var that = this;
-			var args = Array.prototype.slice.call(arguments, 0);
+        reset: function () {
+            var that = this;
+            var args = Array.prototype.slice.call(arguments, 0);
 
-			return new Promise(function(resolve, reject) {
-				args.push(function(err, rows) {
-					if (err) {
-						reject(err);
-					}
-					resolve(rows);
-				});
+            return new Promise(function (resolve, reject) {
+                args.push(function (err) {
+                    if (err) {
+                        reject(err);
+                    }
+                    resolve();
+                });
 
-				that.statement.all.apply(that.statement, args);
-			});
-		},
-		each: function() {
-			var that = this;
-			var args = Array.prototype.slice.call(arguments, 0);
-			return new Promise(function(resolve, reject) {
-				var lastArgs = args[args.length - 1];
-				if (typeof lastArgs !== 'function') {
-					args.push(function(err, row) {
-						if (err) {
-							reject(err);
-						}
+                that.statement.reset.apply(that.statement, args);
+            });
+        },
 
-						// TODO: how to reslove each row
-					});
-				}
-				// complete callback
-				args.push(function(err, num) {
-					if (err) {
-						reject(err);
-					}
-					resolve(num);
-				});
-				that.statement.each.apply(that.statement, args);
+        finalize: function () {
+            var that = this;
+            var args = Array.prototype.slice.call(arguments, 0);
 
-			});
+            return new Promise(function (resolve, reject) {
+                args.push(function () {
+                    resolve();
+                });
 
-		}
-	};
+                that.statement.finalize.apply(that.statement, args);
+            });
+        },
 
-	// sqlite3 connection
-	return new Promise(function(resolve, reject) {
-		var db = new sqlite3.Database(filename, mode);
+        run: function () {
+            var that = this;
+            var args = Array.prototype.slice.call(arguments, 0);
 
-		db.on('open', function() {
-			//console.log(new CoSqlite3(db).run);
-			resolve(new CoSqlite3(db));
-		});
+            return new Promise(function (resolve, reject) {
+                args.push(function (err) {
+                    if (err) {
+                        reject(err);
+                    }
+                    resolve(this);
+                });
 
-		db.on('error', function(err) {
-			reject(err);
-		});
+                that.statement.run.apply(that.statement, args);
+            });
+        },
 
-		//return db;
-	});
+        get: function () {
+            var that = this;
+            var args = Array.prototype.slice.call(arguments, 0);
+
+            return new Promise(function (resolve, reject) {
+                args.push(function (err, row) {
+                    if (err) {
+                        reject(err);
+                    }
+                    resolve(row);
+                });
+
+                that.statement.get.apply(that.statement, args);
+            });
+        },
+
+        all: function () {
+            var that = this;
+            var args = Array.prototype.slice.call(arguments, 0);
+
+            return new Promise(function (resolve, reject) {
+                args.push(function (err, rows) {
+                    if (err) {
+                        reject(err);
+                    }
+                    resolve(rows);
+                });
+
+                that.statement.all.apply(that.statement, args);
+            });
+        },
+
+        each: function () {
+            var that = this;
+            var args = Array.prototype.slice.call(arguments, 0);
+            return new Promise(function (resolve, reject) {
+                var lastArgs = args[args.length - 1];
+                if (typeof lastArgs !== 'function') {
+                    args.push(function (err, row) {
+                        if (err) {
+                            reject(err);
+                        }
+
+                        // TODO: how to resolve each row?
+                    });
+                }
+                
+                // complete callback
+                args.push(function (err, num) {
+                    if (err) {
+                        reject(err);
+                    }
+                    resolve(num);
+                });
+                that.statement.each.apply(that.statement, args);
+
+            });
+
+        }
+    };
+
+    // sqlite3 connection
+    return new Promise(function (resolve, reject) {
+        var db = new sqlite3.Database(filename, mode);
+
+        db.on('open', function () {
+            //console.log(new PSQLite3(db).run);
+            resolve(new PSQLite3(db));
+        });
+
+        db.on('error', function (err) {
+            reject(err);
+        });
+
+        //return db;
+    });
 
 };
